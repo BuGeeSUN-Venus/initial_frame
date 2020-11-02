2a79ae216c46a29f2f32c227fd3cb945cc504336
BuGeeSUN-Venus <41279781+BuGeeSUN-Venus@users.noreply.github.com>
2020-11-02 22:06:01 +0800

    日志更改为logback 性能强，长得好看

--- src/main/java/com/example/initial_frame/controller/TestController.java
+++ src/main/java/com/example/initial_frame/controller/TestController.java
@@ -25,7 +25,6 @@ public class TestController {
 
     @RequestMapping(value = "test", method = {RequestMethod.POST})
     public ResponseData test(@Validated(Insert.class) @RequestBody TestBean testBean, BindingResult result) {
-
         ResultResponseUtil.check(result);
         return ResponseData.SUCCESS(null);
     }
